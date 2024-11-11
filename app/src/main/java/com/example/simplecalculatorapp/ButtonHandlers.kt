76ac44b7ee67af43e788eebbdaf4160c955d98af
immedicable <nikolchev98@protonmail.com>
2024-11-11 13:45:11 +0200

    Set number size limit dynamically depending on device screen size.

--- app/src/main/java/com/example/simplecalculatorapp/ButtonHandlers.kt
+++ app/src/main/java/com/example/simplecalculatorapp/ButtonHandlers.kt
@@ -1,5 +1,8 @@
 package com.example.simplecalculatorapp
 
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import android.util.TypedValue
 import android.widget.Button
 import android.widget.TextView
 
@@ -17,13 +20,17 @@ class ButtonHandlers(private val calculationField: TextView, private val resultF
     }
 
     fun onNumberButtonClick(button: Button) {
+        val maxDigits = calculateMaxDigitsToFitScreen(calculationField.textSize)
+
         //Stops the user from entering more than 10 digits before the operator
-        if (!calculationField.text.contains(operatorsRegex) && calculationField.length() >= 10) {
+        if (!calculationField.text.contains(operatorsRegex) && calculationField.length() >= maxDigits) {
             return
         }
 
         //Stops the user from entering more than 10 digits after the operator
-        if (calculationField.text.contains(operatorsRegex) && calculationField.text.split(" ").last().length >= 10) {
+        if (calculationField.text.contains(operatorsRegex) && calculationField.text.split(" ")
+                .last().length >= maxDigits
+        ) {
             return
         }
 
@@ -50,5 +57,24 @@ class ButtonHandlers(private val calculationField: TextView, private val resultF
         if (calculation.isNotEmpty() && !calculation.contains('.')) {
             calculationField.append(".")
         }
+    }
+
+    private fun calculateMaxDigitsToFitScreen(textSizePx: Float): Int {
+        val textSizeSp = convertPixelsToSp(textSizePx)
+        val displayMetrics = Resources.getSystem().displayMetrics
+        val screenWidth = Resources.getSystem().displayMetrics.widthPixels
+
+        val charWidth = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_SP,
+            textSizeSp,
+            displayMetrics
+        )
+
+        return (screenWidth / charWidth).toInt()
+    }
+
+    private fun convertPixelsToSp(px: Float) : Float {
+        val densityDpi = Resources.getSystem().displayMetrics.densityDpi.toFloat()
+        return px * (DisplayMetrics.DENSITY_DEFAULT / densityDpi)
     }
 }
